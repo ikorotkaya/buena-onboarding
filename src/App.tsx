5e2c8bcf0549b7b9762bab631fd12f2604d5089b
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-08 21:42:25 +0100

    Add Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button 
+          className="bg-red-50"
+          onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
