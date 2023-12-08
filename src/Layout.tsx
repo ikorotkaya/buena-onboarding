37696a6e4c01905bc1e590fbf8881df39323969b
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-08 22:59:28 +0100

    Add hero icons and introduce container

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 export default function Layout() {
 
   return (
-    <div>
+    <div className="container">
       <Navbar />
 
       <Outlet />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 }
