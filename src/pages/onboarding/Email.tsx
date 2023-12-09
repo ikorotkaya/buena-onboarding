e716161292b297bb1af489a5202d6b5bb4393e2e
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-09 19:01:16 +0100

    Add style for name screen

--- src/pages/onboarding/Email.tsx
+++ src/pages/onboarding/Email.tsx
@@ -1,28 +1,29 @@
 import OnboardingNavigation from "../../components/OnboardingNavigation";
-
 import { useOnboardingStore } from "../../store";
 
 export default function Email() {
   const { email, setEmail } = useOnboardingStore();
 
   return (
-    <div className="flex flex-col justify-center items-center md:grid jg:gap-3 md:grid-cols-3">      
-      <div className="col-span-2 flex flex-col justify-center items-center">
-        <h2 className="mt-2 text-4xl font-bold text-gray-900 sm:text-5xl">
-          What is your email?
-        </h2>
+    <div className="flex flex-col pt-48 md:max-w-xl">
+      <h2 className="block mb-8 text-4xl font-bold text-gray-900 sm:text-5xl">
+        What is your email?
+      </h2>
 
-        <div className="mt-2">
-          <input
-            defaultValue={email}
-            type="text"
-            placeholder="you@example.com"
-            className="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6 mb-4"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <OnboardingNavigation />
+      <div className="mb-4">
+        <input
+          defaultValue={email}
+          type="text"
+          placeholder="you@example.com"
+          className="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 
+            shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+            focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6 mb-4"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
+      
+      <OnboardingNavigation
+        className="hidden md:flex" />
     </div>
   );
 }
--- src/pages/onboarding/Name.tsx
+++ src/pages/onboarding/Name.tsx
@@ -16,8 +16,8 @@ export default function Name() {
           type="text"
           placeholder="Jane Smith"
           className="block w-full rounded-md border-0 px-4 py-1.5 text-gray-900 
-          shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
-          focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
+            shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+            focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
           onChange={(e) => setName(e.target.value)}
         />
       </div>
