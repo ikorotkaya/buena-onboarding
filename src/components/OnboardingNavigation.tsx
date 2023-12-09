ba46d31f288f1d5558457ca8d7bc31320371cc85
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-09 21:24:43 +0100

    Remove back button from desktop navigation

--- src/components/OnboardingNavigation.tsx
+++ src/components/OnboardingNavigation.tsx
@@ -7,39 +7,13 @@ import SubmitButton from "./Submitbutton";
 export default function OnboardingNavigation({ className = "" }: { className?: string}) {
   const { currentPageIndex } = useOnboardingStore();
   const [nextPage, setNextPage] = useState(pages[currentPageIndex + 1]);
-  const [previousPage, setPreviousPage] = useState(pages[currentPageIndex - 1]);
 
   useEffect(() => {
     setNextPage(pages[currentPageIndex + 1]);
-    setPreviousPage(pages[currentPageIndex - 1]);
   }, [currentPageIndex]);
 
   return (
     <div className={`flex flex-row justify-between gap-4 px-8 py-4 md:p-0 w-full ${className}`}>
-      {/* {previousPage && (
-        <Link
-          to={`/onboarding/${previousPage}`}
-          className="hidden md:flex items-center justify-center px-4 py-2 text-base font-medium
-           text-gray-500 whitespace-no-wrap bg-white border-2 rounded-full shadow-md hover:bg-black hover:text-white hover:border-black focus:outline-none"
-        >
-          <svg
-            className="w-4 h-4 mr-2"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 19l-7-7m0 0l7-7m-7 7h18"
-            ></path>
-          </svg>
-          Back
-        </Link>
-      )} */}
-
       {nextPage && (
         <Link
           to={`/onboarding/${nextPage}`}
