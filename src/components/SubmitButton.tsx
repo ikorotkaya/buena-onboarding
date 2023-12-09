f72af595cd790d37041744ea82ac889182d620e5
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-09 21:30:44 +0100

    Disable submit button when not all data is collected

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,12 +1,20 @@
+import { useOnboardingStore } from "../store";
 import { Link } from "react-router-dom";
 
 export default function SubmitButton() {
+  const { name, email, phoneNumber, income } = useOnboardingStore();
+
+  const buttonEnabled = name && email && phoneNumber && income;
+
   return (
     <>
       <Link
         to="/final-page"
 
-        className="relative w-full md:w-auto inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium transition duration-300 ease-out border-2 border-yellow-500 rounded-full shadow-md group"
+        className={`relative w-full md:w-auto inline-flex items-center 
+          justify-center p-4 px-6 py-3 overflow-hidden font-medium 
+          transition duration-300 ease-out border-2 border-yellow-500 
+          rounded-full shadow-md group ${buttonEnabled ? "cursor-pointer text-yellow-500" : "bg-gray-100 border-gray-500 text-gray-500 pointer-events-none"}`}
       >
         <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-yellow-500 group-hover:translate-x-0 ease">
           <svg
@@ -24,7 +32,7 @@ export default function SubmitButton() {
             ></path>
           </svg>
         </span>
-        <span className="absolute flex items-center justify-center w-full h-full text-yellow-500 transition-all duration-300 transform group-hover:translate-x-full ease">
+        <span className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform group-hover:translate-x-full ease">
           Submit
         </span>
         <span className="relative invisible">Submit</span>
