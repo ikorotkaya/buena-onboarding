76bdb166fbcebc75b9662d43fac2955f69d1ee88
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-11 23:17:43 +0100

    Write e2e specs from user perspective

--- cypress/e2e/onboarding-flow.cy.ts
+++ cypress/e2e/onboarding-flow.cy.ts
@@ -2,42 +2,32 @@ describe("Onboarding Flow", () => {
   it("should navigate through the onboarding flow", () => {
     cy.visit("http://localhost:5173");
 
-    cy.get("h1").should("contain.text", "Welcome to Buena");
-    cy.get(".subheadline").should(
-      "contain.text",
-      "Enter your personal details and start your journey today."
-    );
-    cy.get(`[data-testid="homepage-cta"]`).should(
-      "contain.text",
-      "Get started"
-    );
-    cy.get(`[data-testid="homepage-cta"]`).click();
+    cy.contains("Welcome to Buena");
+    cy.contains("Enter your personal details and start your journey today.");
+
+    cy.get('a:contains(Get started)').click()
 
     cy.url().should("eq", "http://localhost:5173/onboarding/name");
 
-    cy.get("h1").should("contain.text", "What is your full name?");
-    cy.get("input").should("have.value", "");
-    cy.get(`[data-testid="input-error"]`).should("not.exist");
-    cy.get(`[data-testid="next-button"]`).should("contain.text", "Next");
-    cy.get(`[data-testid="next-button"]`).should(
-      "have.class",
-      "pointer-events-none"
-    );
+    cy.contains("What is your full name?");
+    cy.contains("Please enter a valid name.").should('not.exist');
 
-    cy.get("input").type("J");
-    cy.get(`[data-testid="input-error"]`).should("be.visible");
-    cy.get("input").clear().type("Jane Smith");
-    cy.get(`[data-testid="input-error"]`).should("not.exist");
-    cy.get("input").should("have.value", "Jane Smith");
-    cy.get(`[data-testid="next-button"]:visible`).should(
-      "not.have.class",
-      "pointer-events-none"
-    );
-    cy.get(`[data-testid="next-button"]:visible`).click();
+    const nextButton = cy.get('a:contains(Next):visible')
+    nextButton.should("have.css", "pointer-events", "none")
+
+    const inputField = cy.get("input")
+    
+    inputField.type("J");
+    cy.contains("Please enter a valid name.")
+
+    inputField.clear().type("Jane Smith");
+    cy.contains("Please enter a valid name.").should('not.exist');
+    
+    nextButton.click();
 
     cy.url().should("eq", "http://localhost:5173/onboarding/email");
 
-    cy.get("h1").should("contain.text", "What is your email?");
+    cy.contains("What is your email?");
     cy.get("input").should("have.value", "");
     cy.get(`[data-testid="input-error"]`).should("not.exist");
     cy.get(`[data-testid="next-button"]`).should("contain.text", "Next");
@@ -59,7 +49,7 @@ describe("Onboarding Flow", () => {
 
     cy.url().should("eq", "http://localhost:5173/onboarding/phone");
 
-    cy.get("h1").should("contain.text", "What is your phone number?");
+    cy.contains("What is your phone number?");
     cy.get("input").should("have.value", "");
     cy.get(`[data-testid="input-error"]`).should("not.exist");
     cy.get(`[data-testid="next-button"]`).should("contain.text", "Next");
@@ -81,8 +71,8 @@ describe("Onboarding Flow", () => {
 
     cy.url().should("eq", "http://localhost:5173/onboarding/income");
 
-    cy.get("h1").should("contain.text", "What is your income?");
-    cy.get('[type="radio"]').each(($radio) => {
+    cy.contains("What is your income?");
+    cy.get("[type='radio']").each(($radio) => {
       cy.wrap($radio).should("be.visible");
     });
     cy.get(`[data-testid="input-error"]`).should("not.exist");
@@ -92,7 +82,7 @@ describe("Onboarding Flow", () => {
       "pointer-events-none"
     );
 
-    cy.get('[type="radio"]').each(($radio) => {
+    cy.get("[type='radio']").each(($radio) => {
       if ($radio.val() === "4000-plus") {
         cy.wrap($radio).check();
       }
@@ -105,7 +95,7 @@ describe("Onboarding Flow", () => {
 
     cy.url().should("eq", "http://localhost:5173/onboarding/summary");
 
-    cy.get("h1").should("contain.text", "Check your info:");
+    cy.contains("Check your info:");
     cy.get(`[data-testid="summary"]`).should("contain.text", "Jane Smith");
     cy.get(`[data-testid="summary"]`).should("contain.text", "hi@hello.com");
     cy.get(`[data-testid="summary"]`).should("contain.text", "1234567890");
@@ -118,8 +108,8 @@ describe("Onboarding Flow", () => {
 
     cy.url().should("eq", "http://localhost:5173/final-page");
 
-    cy.get("h1").should("contain.text", "Thank you for your submission!");
-    cy.get('[data-testid="submission-confirmation"]').should(
+    cy.contains("Thank you for your submission!");
+    cy.get("[data-testid='submission-confirmation']").should(
       "contain.text",
       "We will be in touch shortly."
     );
