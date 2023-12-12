4b6ad243457bd38d56c7b146832ba9bea52636ff
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-12 19:18:31 +0100

    Add Jest test coverage report

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['./src/setupTests.ts']
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFiles: ["./src/setupTests.ts"],
+
+  // coverageDirectory: "<rootDir>/coverage"
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**"],
+  coverageThreshold: {
+    global: {
+      lines: 80
+    }
+  }
 };
