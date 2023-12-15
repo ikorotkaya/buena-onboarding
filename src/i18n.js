979ddccd276a2eeaf47815aebf5fa4da2f3dd059
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-15 22:30:41 +0100

    Add i18next packages

---
+++ src/i18n.js
@@ -0,0 +1,33 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import translationEn from "./locales/en/translationEn.json";
+import translationDe from "./locales/de/translationDe.json";
+
+const resources = {
+  en: {
+    translation: translationEn
+  },
+  de: {
+    translation: translationDe
+  }
+};
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "en",
+    debug: true,
+    lng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
