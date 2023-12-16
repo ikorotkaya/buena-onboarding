4792513a00a0a7d96c729e581da199be548b4818
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-16 22:02:27 +0100

    Add input autofocus

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -2,6 +2,7 @@ export interface TextInputProps {
   placeholder?: string;
   type?: string;
   defaultValue?: string;
+  autoFocus?: boolean;
   onChange: (value: string) => void;
 }
 
@@ -9,7 +10,8 @@ export default function TextInput({
   onChange,
   placeholder = "",
   type = "text",
-  defaultValue = ""
+  defaultValue = "",
+  autoFocus = true
 }: TextInputProps) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange(e.target.value);
@@ -18,6 +20,7 @@ export default function TextInput({
   return (
     <input
       defaultValue={defaultValue}
+      autoFocus={autoFocus}
       type={type}
       placeholder={placeholder}
       className="block w-full rounded-md border-0 px-4 py-1.5 text-stone-900 
