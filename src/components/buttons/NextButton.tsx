b53a061142ba34de61d45364f4b8b62c67bb3cca
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-17 12:20:52 +0100

    Add custom shake animation to disabled Next button

--- src/components/buttons/NextButton.tsx
+++ src/components/buttons/NextButton.tsx
@@ -20,21 +20,27 @@ export default function NextButton({
       className={`flex w-full flex-row justify-between gap-4 px-8 py-4 md:p-0 ${className}`}
     >
       {nextOnboardingPage && (
-        <Link
-          to={`/onboarding/${nextOnboardingPage}`}
-          data-testid="next-button"
-          className={`flex w-full items-center justify-center whitespace-nowrap rounded-md 
-            px-6 py-3 text-base font-medium text-white md:ml-auto md:w-auto
-            ${
-              disabled
-                ? "pointer-events-none bg-neutral-400"
-                : "bg-neutral-900  focus:outline-none md:hover:bg-neutral-700"
-            }
-          `}
+        <div
+          className={`w-full md:ml-auto md:w-auto ${
+            disabled ? "hover:animate-shake" : ""
+          }`}
         >
-          {t("nextButton")}
-          <ArrowRightIcon className="ml-2" />
-        </Link>
+          <Link
+            to={`/onboarding/${nextOnboardingPage}`}
+            data-testid="next-button"
+            className={`flex  items-center justify-center whitespace-nowrap 
+              rounded-md px-6 py-3 text-base font-medium text-white 
+              ${
+                disabled
+                  ? "pointer-events-none bg-neutral-400"
+                  : "bg-neutral-900  focus:outline-none md:hover:bg-neutral-700"
+              }
+            `}
+          >
+            {t("nextButton")}
+            <ArrowRightIcon className="ml-2" />
+          </Link>
+        </div>
       )}
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,27 @@ export default {
     container: {
       center: true,
     },
-    
+    extend: {
+      animation: { // https://tailwindcss.com/docs/animation#customizing-your-theme
+        shake: 'shake 1s ease-in-out', // https://css-tricks.com/snippets/css/shake-css-keyframe-animation/
+      },
+      keyframes: {
+        shake: {
+          '10%, 90%': {
+            transform: 'translateX(-1px)',
+          },
+          '20%, 80%': {
+            transform: 'translateX(2px)',
+          },
+          '30%, 50%, 70%': {
+            transform: 'translateX(-4px)',
+          },
+          '40%, 60%': {
+            transform: 'translateX(4px)',
+          },
+        }
+      }
+    }
   },
   plugins: [
     forms,
